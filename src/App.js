77e98ee419c18397969ac10f69e69f7cb981fa5d
nikhiluppala <uppala.n@northeastern.edu>
2024-03-04 20:46:24 -0500

    completed tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
-import Header1 from './components/Header1';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
-import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
+import React from 'react';
+import Header1 from './components/Header1'
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
 
 function App() {
   return (
     <GlobalProvider>
       <Header1 />
-      <div className='container'>
+      <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-      <IncomeExpenses />
-      <TransactionList />
-      <AddTransaction />
-
     </GlobalProvider>
   );
 }
