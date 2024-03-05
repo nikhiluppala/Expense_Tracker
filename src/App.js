0e50c0822e1af20b6feb5019103771cbd193b346
nikhiluppala <uppala.n@northeastern.edu>
2024-03-04 20:34:24 -0500

    Worked on Balance components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import Header1 from './components/Header1';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
-
-
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header1 />
       <div className='container'>
         <Balance />
       </div>
       <IncomeExpenses />
       <TransactionList />
+      <AddTransaction />
 
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import Transaction from './Transaction';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+
+const {transactions} = useContext(GlobalContext);
+
   return (
     <>
-        <h3>History</h3>
-        <ul id='list' className='list'>
-            <li>
-                Cash <span>-$400</span><button className='delete-btn'>x</button>
-            </li>
-        </ul>
-    </>
+    <h3>History</h3>
+    <ul className="list">
+      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+    </ul>
+  </>
   )
 }
 
